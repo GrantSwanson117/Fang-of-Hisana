c20a22834eff234aa0a5979590a3cc7ce46d95f7
PaladinDigital117 <gswaner77@gmail.com>
2023-12-21 17:42:55 -0800

    Added player art and animations

--- fang.gd
+++ fang.gd
@@ -1,10 +1,21 @@
 extends CharacterBody2D
 
-var speed = 150
+var speed = 75
 var motion = Vector2.ZERO
+var canIdle = false
+@onready var animator = $AnimationPlayer
+@onready var tree = $AnimationTree
+var input_vector = Vector2.ZERO
+@export var attackWindow = false
+
+func _ready():
+	tree.active = true
+	attackWindow = false
+	
+func _process(_delta):
+	updateAnimParameters()
 
 func _physics_process(_delta):
-	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	input_vector = input_vector.normalized()
@@ -15,19 +26,28 @@ func _physics_process(_delta):
 	move_and_slide()
 	motion = velocity
 
-func _on_Attack_body_entered(body):
-	if body.is_in_group("enemies"):
-		body.state = body.HIT
-
-func _on_Attack_body_exited(body):
-	if body.is_in_group("enemies"):
-		body.state = body.SURROUND
-
-func _on_Attract_body_entered(body):
-	if body.is_in_group("enemies"):
-		body.attack_timer.start()
-
-func _on_Attract_body_exited(body):
-	if body.is_in_group("enemies"):
-		body.attack_timer.stop()
-		body.state = body.SURROUND
+func updateAnimParameters():
+	if motion == Vector2.ZERO:
+		tree["parameters/conditions/idle"] = true
+		tree["parameters/conditions/isMoving"] = false
+	else:
+		tree["parameters/conditions/idle"] = false
+		tree["parameters/conditions/isMoving"] = true
+		if motion.x > 0: 
+			$Sprite2D.flip_h = false
+			$Hitbox/CollisionShape2D.position.x = 20
+		if motion.x < 0: 
+			$Sprite2D.flip_h = true
+			$Hitbox/CollisionShape2D.position.x = -20
+	if Input.is_action_just_pressed("attack"):
+		tree["parameters/conditions/attack"] = true
+		tree["parameters/conditions/idle"] = false
+		if attackWindow == true:
+			if animator.current_animation == "Attack 1":
+				animator.play("Attack 2")
+			if animator.current_animation == "Attack 2":
+				animator.play("Attack 1")
+			tree["parameters/conditions/idle"] = false
+	else:
+		tree["parameters/conditions/attack"] = false
+		#tree["parameters/conditions/combo"] = false
