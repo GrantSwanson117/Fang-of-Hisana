82335528f3e3fc77593387031b69eee14be63d2f
PaladinDigital117 <gswaner77@gmail.com>
2024-02-26 22:59:27 -0800

    Added Dust

--- Scripts/entity.gd
+++ Scripts/entity.gd
@@ -11,14 +11,14 @@ var damage: int
 
 func takeDamage(amount: int):
 	health -= amount
-	print(amount)
+	print(health)
 
 func dealDamage(amount: int, target: CharacterBody2D):
 	if target == null:
 		return
 	else:
 		if target.has_method("takeDamage") and target.get_groups() != get_groups():
-			target.takeDamage(damage)
+			target.takeDamage(amount)
 			if target.health <= 0:
 				target.health = 0
 				if target.has_method("die"): target.die()
--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,3 +2,13 @@ extends Node2D
 
 func _ready():
 	pass
+
+func dealDamage(amount: int, target: CharacterBody2D):
+	if target == null:
+		return
+	else:
+		if target.has_method("takeDamage") and target.get_groups() != get_groups():
+			target.takeDamage(amount)
+			if target.health <= 0:
+				target.health = 0
+				if target.has_method("die"): target.die()
